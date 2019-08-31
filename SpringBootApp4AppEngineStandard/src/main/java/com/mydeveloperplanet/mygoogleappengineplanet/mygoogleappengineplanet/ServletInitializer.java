8e8dbb78b093a48bea99082b611474e25e83c281
satya <satya.iton@gmail.com>
2019-08-31 08:30:27 +0530

    SpringBoot Application for GCP App Engine for Standard Development

---
+++ SpringBootApp4AppEngineStandard/src/main/java/com/mydeveloperplanet/mygoogleappengineplanet/mygoogleappengineplanet/ServletInitializer.java
@@ -0,0 +1,11 @@
+package com.mydeveloperplanet.mygoogleappengineplanet.mygoogleappengineplanet;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MyGoogleAppEnginePlanetApplication.class);
+    }
+}
